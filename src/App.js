c3f9b6253d57b46b823506f0c62bc7168f83fe03
KittyLore <141794841+KittyLore@users.noreply.github.com>
2025-04-01 20:06:07 -0500

    1
    App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//  Contenido de la pagina
+
+function App()
+{
+    const value = 'world';
+    return <div> Hello {value}</div>;
+    
+
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Se realiza un import de un archivo llamado App que se comporta como un componente
+
+import App from './App'
+
+ReactDOM.render(<App/>, 
+    document.getElementById('root'));
