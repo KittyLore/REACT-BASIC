1e51e1c510332e236cca567c8c256b39fbd13a05
KittyLore <141794841+KittyLore@users.noreply.github.com>
2025-04-01 20:57:09 -0500

    1
    rrfggg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from  'react';
 import ReactDOM from 'react-dom';
 
 // Se realiza un import de un archivo llamado App que se comporta como un componente
 
 import App from './App'
 
-ReactDOM.render(<App/>, 
-    document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
